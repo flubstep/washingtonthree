66f89f8d8cf6f42adb5d4a01315e62bf262e6778
Albert Sheu <albert.sheu@gmail.com>
2023-05-02 18:17:07 -0400

    Replace parcel with vite

--- src/constants.js
+++ src/constants.js
@@ -2,7 +2,8 @@ export const [XMIN, XMAX] = [389400, 408600];
 export const [YMIN, YMAX] = [124200, 148200];
 export const CX = (XMIN + XMAX) / 2;
 export const CY = (YMIN + YMAX) / 2;
-export const BASE_URL = "https://s3.us-east-2.wasabisys.com/washingtonthree/bintiles";
+export const BASE_URL = "https://s3.us-east-2.wasabisys.com/washingtonthree/bintiles_resampled";
+//export const BASE_URL = "http://localhost:8080/bintiles_resampled";
 
 export const HEIGHT_MAP_TEXTURE_URL =
   "https://s3.us-east-2.wasabisys.com/washingtonthree/textures/ground_heightmap_small.png";
