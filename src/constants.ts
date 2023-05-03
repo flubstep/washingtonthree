cc4b8069fb1c6ddd452f064a138946a75046fef4
Albert Sheu <albert.sheu@gmail.com>
2023-05-02 20:13:02 -0400

    Use localhost for development mode only

--- src/constants.ts
+++ src/constants.ts
@@ -2,8 +2,11 @@ export const [XMIN, XMAX] = [389400, 408600];
 export const [YMIN, YMAX] = [124200, 148200];
 export const CX = (XMIN + XMAX) / 2;
 export const CY = (YMIN + YMAX) / 2;
-//export const BASE_URL = "https://s3.us-east-2.wasabisys.com/washingtonthree/bintiles_resampled";
-export const BASE_URL = "http://localhost:8080/bintiles_resampled";
+
+// @ts-ignore
+export const BASE_URL = import.meta.env.DEV
+  ? "http://localhost:8080/bintiles_resampled"
+  : "https://s3.us-east-2.wasabisys.com/washingtonthree/bintiles_resampled";
 
 export const HEIGHT_MAP_TEXTURE_URL =
   "https://s3.us-east-2.wasabisys.com/washingtonthree/textures/ground_heightmap_small.png";
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 // vite.config.js
-import string from 'vite-plugin-string'
+import string from "vite-plugin-string";
 
 export default {
   plugins: [
     string({
-      include: '**/*.glsl', // You can specify any pattern to match your .glsl files
+      include: "**/*.glsl", // You can specify any pattern to match your .glsl files
     }),
   ],
-}
+};
